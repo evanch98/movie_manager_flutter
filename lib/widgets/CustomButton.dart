788e6c12036744f261445f208a03fe02f51b9be8
Kyaw Thu <evanch98@gmail.com>
2022-07-06 22:39:33 +0800

    Adding comments for CustomButton()

--- lib/widgets/CustomButton.dart
+++ lib/widgets/CustomButton.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:movie_manager_flutter/constants.dart';
 
+/*
+CustomButton widget accepts has two parameters.
+buttonName: Accepts String value as an argument and displays the value
+            on the button.
+onPressed: Accepts Function as an argument.
+ */
 class CustomButton extends StatelessWidget {
   const CustomButton({
     Key? key,
@@ -14,9 +20,11 @@ class CustomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
+      // to give the height value of the button.
       height: 50.0,
       child: ElevatedButton(
         onPressed: onPressed,
+        // Styling the ElevatedButton widget.
         style: ElevatedButton.styleFrom(
             primary: kRodeoDust,
             elevation: 5,
