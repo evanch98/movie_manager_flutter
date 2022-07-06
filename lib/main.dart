04b82accbef5a9648451615aa2c212ec90e88e3c
Kyaw Thu <evanch98@gmail.com>
2022-07-06 21:05:23 +0800

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MovieManager());
+}
+
+class MovieManager extends StatelessWidget {
+  const MovieManager({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
