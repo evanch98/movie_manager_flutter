c13e117c2ecf9e90de6138228764d3a9f95c8e91
Kyaw Thu <evanch98@gmail.com>
2022-07-06 22:08:19 +0800

    Initial setup for the LogInScreen

--- lib/screens/logInScreen.dart
+++ lib/screens/logInScreen.dart
@@ -11,6 +11,33 @@ class LogInScreen extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
       backgroundColor: kPlatinum,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 14.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'images/mm.png',
+              width: kLogoSize,
+              height: kLogoSize,
+            ),
+            const SizedBox(
+              height: 17.0,
+            ),
+            const Text(
+              'Movie Manager',
+              style: kTitleTextStyle,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 70.0,
+            ),
+          ],
+        ),
+      ),
     ));
   }
 }
