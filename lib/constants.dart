eb250c67071f8ce032bb816a78cfb339a3c86042
Kyaw Thu <evanch98@gmail.com>
2022-07-06 21:25:40 +0800

    Adding constant values in constants.dart file

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPlatinum = Color(0xFFE6E6E6);
+const kRodeoDust = Color(0xFFC5A880);
+const kIndianTan = Color(0xFF532E1C);
+const kBlack = Color(0xFF0F0F0F);
