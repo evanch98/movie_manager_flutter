6d9327b9ba4eac442e4b41759c952c7c51c66d27
Kyaw Thu <evanch98@gmail.com>
2022-07-08 14:31:18 +0800

    Adding a comment describing the purpose of the file in constants.dart file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+/* Every constant value for this application can be found in this file. */
+
 const kLogoSize = 126.0;
 
 const kPlatinum = Color(0xFFE6E6E6);
