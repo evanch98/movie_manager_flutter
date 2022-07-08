b36c62216b7f6186f3824e12b58b61ad2b9f821f
Kyaw Thu <evanch98@gmail.com>
2022-07-08 21:55:35 +0800

    Updating MovieCard

--- lib/widgets/MovieCard.dart
+++ lib/widgets/MovieCard.dart
@@ -5,35 +5,43 @@ class MovieCard extends StatelessWidget {
   const MovieCard({
     Key? key,
     this.image,
+    this.onTap,
+    this.onLongPress,
     required this.movieTitle,
   }) : super(key: key);
 
   final Image? image;
   final String movieTitle;
+  final VoidCallback? onTap;
+  final VoidCallback? onLongPress;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 150.0,
-          width: 100.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            color: kRodeoDust,
+    return GestureDetector(
+      onTap: onTap,
+      onLongPress: onLongPress,
+      child: Column(
+        children: [
+          Container(
+            height: 150.0,
+            width: 100.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              color: kRodeoDust,
+            ),
+            child: Center(
+              child: image ?? const Text('No Image'),
+            ),
           ),
-          child: Center(
-            child: image ?? const Text('No Image'),
+          const SizedBox(
+            height: 10.0,
           ),
-        ),
-        const SizedBox(
-          height: 10.0,
-        ),
-        Text(
-          movieTitle,
-          textAlign: TextAlign.center,
-        ),
-      ],
+          Text(
+            movieTitle,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
