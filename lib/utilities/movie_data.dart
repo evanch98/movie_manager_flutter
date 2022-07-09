2531052010a9f692ab10087d8c56afffc5b6b9f3
Kyaw Thu <evanch98@gmail.com>
2022-07-09 20:13:30 +0800

    Adding Favorite and Watched List

--- lib/utilities/movie_data.dart
+++ lib/utilities/movie_data.dart
@@ -4,6 +4,8 @@ import '/utilities/movie.dart';
 
 class MovieData extends ChangeNotifier {
   List<Movie> toWatch = [];
+  List<Movie> watched = [];
+  List<Movie> favorite = [];
 
   void addToWatch(String movieTitle, FileImage image) {
     toWatch.add(
@@ -19,8 +21,26 @@ class MovieData extends ChangeNotifier {
     return toWatch.length;
   }
 
+  int get watchedCount {
+    return watched.length;
+  }
+
+  int get favoriteCount {
+    return favorite.length;
+  }
+
   void removeFromToWatch(Movie movie) {
     toWatch.remove(movie);
+    notifyListeners();
+  }
+
+  void removeFromWatched(Movie movie) {
+    watched.remove(movie);
+    notifyListeners();
+  }
+
+  void removeFromFavorite(Movie movie) {
+    favorite.remove(movie);
     notifyListeners();
   }
 }
