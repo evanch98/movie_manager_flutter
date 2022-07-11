9fdf69a6f041ebedea5cd5e38a767f501de9c8f3
Kyaw Thu <evanch98@gmail.com>
2022-07-11 23:53:48 +0800

    Adding comments

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -8,6 +8,11 @@ import '../constants.dart';
 import '../widgets/custom_button.dart';
 import '../widgets/custom_text_field.dart';
 
+/*
+* RegisterScreen is a StatefulWidget that allows users to log in their account.
+* LogInScreen consists of CustomTextField and CustomButton, and they can be
+* found in "./lib/widgets/." The logo is wrapped in a Hero widget for animation.
+*/
 class RegisterScreen extends StatefulWidget {
   static String id = 'RegisterScreen';
 
@@ -82,6 +87,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
             CustomButton(
                 buttonName: 'Register',
                 onPressed: () async {
+                  // To allow users to register an account.
                   try {
                     final newUser = await _auth.createUserWithEmailAndPassword(
                       email: _email,
@@ -94,9 +100,12 @@ class _RegisterScreenState extends State<RegisterScreen> {
                         ),
                       );
                       if (!mounted) return;
+                      // If nothing bad happens, will navigate to
+                      // UsernameScreen()
                       ScaffoldMessenger.of(context).showSnackBar(snackBar);
                       Navigator.pushNamed(context, UsernameScreen.id);
                     }
+                    // To catch any allow that will come while registering.
                   } on FirebaseAuthException catch (e) {
                     SnackBar snackBar = const SnackBar(
                       content: Text('Something went wrong. Please try again'),
