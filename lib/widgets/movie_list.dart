e9578784775865a9cd9d79a5313e81b63d00eb67
Kyaw Thu <evanch98@gmail.com>
2022-07-11 16:32:19 +0800

    Updating codes so that the list will come from cloud firestore

--- lib/widgets/movie_list.dart
+++ lib/widgets/movie_list.dart
@@ -1,11 +1,8 @@
 // Importing required packages
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:movie_manager_flutter/constants.dart';
-import 'package:provider/provider.dart';
 
-// Importing required modules
-import '/utilities/movie_data.dart';
-import '../utilities/movie.dart';
 import '../widgets/movie_card.dart';
 
 /* MovieList is a StatelessWidget for building a ListView for MovieCard.
@@ -17,60 +14,50 @@ import '../widgets/movie_card.dart';
  * The MovieList has one required field.
  *     list: To specify which list.
 */
-class MovieList extends StatelessWidget {
+class MovieList extends StatefulWidget {
   const MovieList({
     Key? key,
     required this.list,
   }) : super(key: key);
 
   final String list;
+
+  @override
+  State<MovieList> createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  final _auth = FirebaseAuth.instance;
+  final _fireStore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<MovieData>(
-      builder: (context, movieData, child) {
-        // To get the length of the specified list.
-        int? listCount;
+    return Column(
+      children: [
+        StreamBuilder<QuerySnapshot>(
+          stream: _fireStore
+              .collection('${_auth.currentUser?.uid}_${widget.list}')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final movies = snapshot.data!.docs;
+              List<MovieCard> movieCards = [];
+              for (var movie in movies) {
+                final title = movie['title'];
+                final image = NetworkImage(movie['image']);
 
-        // To change the listCount according to the list value.
-        if (list == kWatched) {
-          listCount = movieData.watchedCount;
-        } else if (list == kFavorite) {
-          listCount = movieData.favoriteCount;
-        } else if (list == kToWatch) {
-          listCount = movieData.toWatchCount;
-        }
-        return SizedBox(
-          // The height of the SizedBox will change according to length of the
-          // list. If the length > 0, 180 else 0.
-          height: listCount! > 0 ? 180.0 : 0.0,
-          child: ListView.builder(
-            padding: EdgeInsets.zero,
-            itemBuilder: (context, index) {
-              // To get the data from the specified list.
-              late Movie movie;
-              // To change the list where movie get Movie objects.
-              if (list == kWatched) {
-                movie = movieData.watched[index];
-              } else if (list == kFavorite) {
-                movie = movieData.favorite[index];
-              } else if (list == kToWatch) {
-                movie = movieData.toWatch[index];
+                final movieCard = MovieCard(image: image, movieTitle: title);
+                movieCards.add(movieCard);
               }
-              // To return a list of MovieCards.
-              return MovieCard(
-                image: movie.image,
-                movieTitle: movie.movieTitle,
-                onLongPress: () {
-                  movieData.removeFromList(movie, list);
-                },
+              return Row(
+                children: movieCards,
               );
-            },
-            itemCount: listCount,
-            // To display the list horizontally.
-            scrollDirection: Axis.horizontal,
-          ),
-        );
-      },
+            } else {
+              return Row();
+            }
+          },
+        ),
+      ],
     );
   }
 }
