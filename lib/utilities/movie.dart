654a1b1fcd6a18bd55279773f92963b6c4a16e19
Kyaw Thu <evanch98@gmail.com>
2022-07-12 16:23:38 +0800

    Deleted an unnecessary file

--- lib/utilities/movie.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Movie {
-  late final String movieTitle;
-  late final FileImage image;
-
-  Movie({required this.movieTitle, required this.image});
-}
